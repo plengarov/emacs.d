d5a4b34f63cc93bdbada95a74c7d2de1e02d8e3b
Pavel Lengarov <pavel.lengarov@gmail.com>
2019-12-18 16:19:01 +0200

    Some unused stuff was removed.

--- personal.el
+++ personal.el
@@ -34,14 +34,12 @@
 ;;; Code:
 (defvar package-list)
 (setq package-list '(ag
-                     irony
                      clang-format
-                     company-rtags
-                     company-irony
-                     company-irony-c-headers
-                     ;; company-cmake
+                     flycheck
                      modern-cpp-font-lock
-                     ggtags))
+                     ggtags
+                     org
+                     org-bullets))
 
 ;; fetch the list of packages available
 (unless package-archive-contents
@@ -67,67 +65,16 @@
 (global-set-key (kbd "C-c C-v") 'clang-format-region)
 (global-set-key (kbd "C-c C-f") 'clang-format-buffer)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Set up code completion with company and irony
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enable semantics mode for auto-completion
 (require 'cc-mode)
 (require 'semantic)
-;; (global-semanticdb-minor-mode 1)
-;; (global-semantic-idle-scheduler-mode 1)
-;; (semantic-mode 1)
-
-;; Setup irony-mode to load in c-modes
-(require 'irony)
-(require 'company-irony-c-headers)
-;;(require 'cl)
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
-
-;; irony-mode hook that is called when irony is triggered
-(defun my-irony-mode-hook ()
-  "Custom irony mode hook to remap keys."
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
-;; company-irony setup, c-header completions
-(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-;; Remove company-semantic because it has higher precedance than company-clang
-;; Using RTags completion is also faster than semantic, it seems. Semantic
-;; also provides a bunch of technically irrelevant completions sometimes.
-;; All in all, RTags just seems to do a better job.
-(setq company-backends (delete 'company-semantic company-backends))
-;; Enable company-irony and several other useful auto-completion modes
-;; We don't use rtags since we've found that for large projects this can cause
-;; async timeouts. company-semantic (after company-clang!) works quite well
-;; but some knowledge some knowledge of when best to trigger is still necessary.
-(eval-after-load 'company
-  '(add-to-list
-    'company-backends '(company-irony-c-headers
-                        company-irony
-                        company-clang
-                        )
-    )
-)
-
-;; Prohibit semantic from searching through system headers. We want
-;; company-clang to do that for us.
-(setq-mode-local c-mode semanticdb-find-default-throttle
-                 '(local project unloaded recursive))
-(setq-mode-local c++-mode semanticdb-find-default-throttle
-                 '(local project unloaded recursive))
-
+(global-semanticdb-minor-mode 1)
+(global-semantic-idle-scheduler-mode 1)
+(semantic-mode 1)
 (semantic-remove-system-include "/usr/include/" 'c++-mode)
 (semantic-remove-system-include "/usr/local/include/" 'c++-mode)
 (add-hook 'semantic-init-hooks
           'semantic-reset-system-include)
-
 
 (require 'ggtags)
 (add-hook 'c-mode-common-hook
@@ -137,12 +84,6 @@
 
 
 (setq whitespace-line-column 110) ;; limit line length
-
-(setq initial-frame-alist `((top . 0) ;; This is overridden by my-center-frame later.
-                           (left . 0) ;; This is overridden by my-center-frame later.
-                           (width . 274) ; character
-                           (height . 80); lines
-                           ))
 
 (require 'modern-cpp-font-lock)
 (modern-c++-font-lock-global-mode t)
@@ -161,7 +102,6 @@
             (org-bullets-mode t)))
 
 (setq org-hide-leading-stars t)
-
 ;; end org mode
 
 ;;; personal.el ends here
